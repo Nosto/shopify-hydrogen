a6ffb78f342eaad4f7774b469ccd13761ead3276
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2023-01-13 14:49:48 +0100

    updated package and vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import react from "@vitejs/plugin-react";
+import hydrogen from "@shopify/hydrogen/plugin";
 import { resolve } from "path";
 import { defineConfig } from "vite";
-
+//
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
@@ -13,13 +14,18 @@ export default defineConfig({
         client: resolve(__dirname, "src/components/index.client.js"),
         server: resolve(__dirname, "src/components/index.server.js"),
       },
+
       name: "@nosto/shopify-hydrogen",
       formats: ["es"],
-      fileName: (format, name) => `index.${name}.js`,
+      fileName: (format, name) =>
+        name == "plugin" ? `${format}/plugin.js` : `${format}/index.${name}.js`,
+      // fileName: (format, name) =>
+      //   name == "plugin" ? `${format}/plugin.js` : `${format}/index.js`,
     },
     rollupOptions: {
       external: ["react", "react-dom", "@shopify/hydrogen"],
       output: {
+        assetName: "test",
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
@@ -33,4 +39,38 @@ export default defineConfig({
     hookTimeout: 10000,
   },
   ssr: { noExternal: ["@nosto/nosto-react"] },
+  // build: {
+  //   rollupOptions: {
+  //     input: {
+  //       client: resolve(__dirname, "src/components/index.client.js"),
+  //       server: resolve(__dirname, "src/components/index.server.js"),
+  //     },
+  //     external: [
+  //       "react",
+  //       "react-dom",
+  //       "@shopify/hydrogen",
+  //       // "@nosto/nosto-react",
+  //     ],
+  //     output: [
+  //       {
+  //         format: "es",
+  //         dir: "dist/es",
+  //         entryFileNames: "entry-[name].js",
+  //         globals: {
+  //           react: "React",
+  //           "react-dom": "ReactDOM",
+  //         },
+  //       },
+  //       {
+  //         format: "cjs",
+  //         dir: "dist/cjs",
+  //         entryFileNames: "entry-[name].js",
+  //         globals: {
+  //           react: "React",
+  //           "react-dom": "ReactDOM",
+  //         },
+  //       },
+  //     ],
+  //   },
+  // },
 });
