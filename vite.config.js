16d814df3c5b37e47f013467d384c515805b4201
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2023-01-13 16:04:17 +0100

    removed test component, edited vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import react from "@vitejs/plugin-react";
-import hydrogen from "@shopify/hydrogen/plugin";
 import { resolve } from "path";
 import { defineConfig } from "vite";
 //
@@ -19,8 +18,6 @@ export default defineConfig({
       formats: ["es"],
       fileName: (format, name) =>
         name == "plugin" ? `${format}/plugin.js` : `${format}/index.${name}.js`,
-      // fileName: (format, name) =>
-      //   name == "plugin" ? `${format}/plugin.js` : `${format}/index.js`,
     },
     rollupOptions: {
       external: ["react", "react-dom", "@shopify/hydrogen"],
@@ -38,39 +35,4 @@ export default defineConfig({
     testTimeout: 10000,
     hookTimeout: 10000,
   },
-  ssr: { noExternal: ["@nosto/nosto-react"] },
-  // build: {
-  //   rollupOptions: {
-  //     input: {
-  //       client: resolve(__dirname, "src/components/index.client.js"),
-  //       server: resolve(__dirname, "src/components/index.server.js"),
-  //     },
-  //     external: [
-  //       "react",
-  //       "react-dom",
-  //       "@shopify/hydrogen",
-  //       // "@nosto/nosto-react",
-  //     ],
-  //     output: [
-  //       {
-  //         format: "es",
-  //         dir: "dist/es",
-  //         entryFileNames: "entry-[name].js",
-  //         globals: {
-  //           react: "React",
-  //           "react-dom": "ReactDOM",
-  //         },
-  //       },
-  //       {
-  //         format: "cjs",
-  //         dir: "dist/cjs",
-  //         entryFileNames: "entry-[name].js",
-  //         globals: {
-  //           react: "React",
-  //           "react-dom": "ReactDOM",
-  //         },
-  //       },
-  //     ],
-  //   },
-  // },
 });
