737b4b5f94b9870d18b7cde5c204f2c2f1ded0fb
Siddharth Sahay <siddharth.sahay@nosto.com>
2023-05-22 15:48:25 +0300

    Add polyfill for Array.at()

--- src/components/client/NostoSession.client.jsx
+++ src/components/client/NostoSession.client.jsx
@@ -1,6 +1,29 @@
 import { NostoSession as NostoComponent } from "@nosto/nosto-react/shopify-hydrogen";
 import { useCart } from "@shopify/hydrogen";
 
+/* Polyfill for older platforms that don't support Array.at()
+  Reference: https://github.com/tc39/proposal-relative-indexing-method#polyfill
+ */
+function at(n) {
+  // ToInteger() abstract op
+  n = Math.trunc(n) || 0;
+  // Allow negative indexing from the end
+  if (n < 0) n += this.length;
+  // OOB access is guaranteed to return undefined
+  if (n < 0 || n >= this.length) return undefined;
+  // Otherwise, this is just normal property access
+  return this[n];
+}
+
+const TypedArray = Reflect.getPrototypeOf(Int8Array);
+for (const C of [Array, String, TypedArray]) {
+  Object.defineProperty(C.prototype, "at",
+      { value: at,
+        writable: true,
+        enumerable: false,
+        configurable: true });
+}
+
 export default function (props) {
   const shopifyCart = useCart();
 
