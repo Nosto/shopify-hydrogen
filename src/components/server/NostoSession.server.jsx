5a75b5d91f4b9b1266743032da81fe4d98dcd97d
Siddharth Sahay <siddharth.sahay@nosto.com>
2023-05-25 13:41:43 +0300

    Support Remix

--- src/components/server/NostoSession.server.jsx
+++ src/components/server/NostoSession.server.jsx
@@ -1,11 +1,34 @@
 import { NostoSessionClient } from "../index.client";
-import { useShopQuery, gql, useSession, useShop } from "@shopify/hydrogen";
+import { gql, useSession, useShop } from "@shopify/hydrogen";
 import { sha256 } from "js-sha256";
+
+export async function loader({params, context}) {
+  const customerAccessToken = await context.session.get('customerAccessToken');
+  const {shop, product} = await context.storefront.query(QUERY, {
+    variables: {
+      customerAccessToken
+    },
+  });
+
+  return json({shop, product});
+}
 
 export default function NostoSession() {
   const { storeDomain } = useShop();
-  const { customerAccessToken } = useSession();
-  const QUERY = gql`
+
+  const {
+    data: { customer: customerData },
+  } = useShopQuery({
+    query: QUERY,
+  });//TODO replace with remix useLoaderData
+
+  if (customerData?.id && storeDomain) {
+    customerData.customerReference = sha256(customerData.id + storeDomain);
+  }
+
+  return <NostoSessionClient customerData={customerData} />;
+}
+const QUERY = gql`
           query {
           customer(customerAccessToken: "${customerAccessToken}") {
             firstName
@@ -16,15 +39,3 @@ export default function NostoSession() {
           }
         }
         `;
-  const {
-    data: { customer: customerData },
-  } = useShopQuery({
-    query: QUERY,
-  });
-
-  if (customerData?.id && storeDomain) {
-    customerData.customerReference = sha256(customerData.id + storeDomain);
-  }
-
-  return <NostoSessionClient customerData={customerData} />;
-}
