bb3169708dbb8fd9ff1bed2ea783d2ea45ceed35
Siddharth Sahay <siddharth.sahay@nosto.com>
2023-05-29 12:33:52 +0300

    Support Remix

--- src/components/client/NostoProvider.client.jsx
+++ src/components/client/NostoProvider.client.jsx
@@ -1,17 +1,23 @@
-import { NostoProvider as NostoComponent } from "@nosto/nosto-react";
-import { useLocalization, fetchSync } from "@shopify/hydrogen";
+import {NostoProvider as NostoComponent} from "@nosto/nosto-react";
+import {useLoaderData} from "@remix-run/react";
+import {json} from "@shopify/remix-oxygen";
+
+export async function loader({context}) {
+  const country = context.storefront.i18n.country
+  return json({country});
+}
 
 export default function (props) {
-  const { country } = useLocalization();
-
-  //Get currentVariation based on country:
+  //Get currentVariation based on country
+  const { country } = useLoaderData()
   const providerProps = {};
   if (props.multiCurrency) {
-    const countries = fetchSync("/api/countries").json() || [];
-    const currentVariation =
-      countries.find((item) => item.isoCode == country.isoCode)?.currency
-        ?.isoCode || "";
-    providerProps.currentVariation = currentVariation;
+    fetch("/api/countries")
+        .then((response) => response.json())
+        .then((countries) => {
+          providerProps.currentVariation = countries.find((item) => item.isoCode === country.isoCode)?.currency
+              ?.isoCode || "";
+        });
   }
 
   return <NostoComponent {...providerProps} {...props} />;
