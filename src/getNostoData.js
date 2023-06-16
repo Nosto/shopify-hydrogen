c917615e8e0050a10ba8295b9e9d052b815a4582
Dominik Gilg <dominikgilg@Doms-MacBook-Pro.local>
2023-06-16 12:29:21 -0600

    added cart fetcher function within getNostoData

--- src/getNostoData.js
+++ src/getNostoData.js
@@ -1,11 +1,11 @@
-export async function getNostoData({ storefront, session }) {
+export async function getNostoData({ context: { storefront, session }, cartId }) {
 
-    //Get store domain:
-    const storeDomain = storefront?.getShopifyDomain?.();
+  //Get store domain:
+  const storeDomain = storefront?.getShopifyDomain?.();
 
-    //Fetch customer data:
-    const customerAccessToken = await session.get('customerAccessToken');
-    const NOSTO_CUSTOMER_QUERY = `#graphql
+  //Fetch customer data:
+  const customerAccessToken = await session.get('customerAccessToken');
+  const NOSTO_CUSTOMER_QUERY = `#graphql
                                   query {
                                     customer(customerAccessToken: "${customerAccessToken}") {
                                       firstName
@@ -16,9 +16,47 @@ export async function getNostoData({ storefront, session }) {
                                     }
                                   }
                               `
-    const customer = await storefront.query(NOSTO_CUSTOMER_QUERY, {
-        cache: storefront.CacheNone(),
-    });
+  const customer = await storefront.query(NOSTO_CUSTOMER_QUERY, {
+    cache: storefront.CacheNone(),
+  });
 
-    return { ...customer, storeDomain }
+  //Fetch cart data:
+  const NOSTO_CART_QUERY = `#graphql
+                              query CartQuery($cartId: ID!, $country: CountryCode, $language: LanguageCode)
+                                @inContext(country: $country, language: $language) {
+                                  cart(id: $cartId) {
+                                    lines(first: 100) {
+                                      edges {
+                                        node {
+                                          quantity
+                                          merchandise {
+                                            ... on ProductVariant {
+                                              id
+                                              price {
+                                                currencyCode
+                                                amount
+                                              }
+                                              product {
+                                                id
+                                              }
+                                            }
+                                          }
+                                        }
+                                      }
+                                    }
+                                  }
+                                }
+                              `;
+  const { cart } = !cartId ? {} : await storefront.query(NOSTO_CART_QUERY, {
+    variables: {
+      cartId,
+      country: storefront.i18n.country,
+      language: storefront.i18n.language,
+    },
+    cache: storefront.CacheNone(),
+  });
+
+  return { ...customer, cart, storeDomain }
 }
+
+
