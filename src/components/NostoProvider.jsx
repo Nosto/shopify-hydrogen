e95c909d440dbbc8d842f30e7e9f93a195a54e48
Dominik Gilg <dominikgilg@Doms-MacBook-Pro.local>
2023-06-22 18:47:40 -0600

    rendering NostoSession within NostoProvider

--- src/components/NostoProvider.jsx
+++ src/components/NostoProvider.jsx
@@ -1,5 +1,12 @@
 import { NostoProvider as NostoComponent } from "@nosto/nosto-react";
+import { NostoSession } from '@nosto/shopify-hydrogen'
 
-export default function (props) {
-  return <NostoComponent {...props} />;
+
+export default function ({ children, ...props }) {
+  return (
+    <NostoComponent {...props}>
+      <NostoSession />
+      {children}
+    </NostoComponent>
+  )
 }
