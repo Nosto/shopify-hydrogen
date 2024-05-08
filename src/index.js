052dd0a1fbdd70b48dc1983fc30a1f5b03cd9b58
Siddharth Sahay <siddharth.sahay@nosto.com>
2024-05-08 13:25:03 +0300

    import correctly

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 
 //Export components:
-export { 
-    Nosto404, 
-    NostoCategory, 
-    NostoCheckout, 
-    NostoHome, 
+export {
+    Nosto404,
+    NostoCategory,
+    NostoCheckout,
+    NostoHome,
     NostoOrder,
     NostoOther,
     NostoPlacement,
     NostoProduct,
     NostoSearch
 } from "@nosto/nosto-react"
-export { NostoProvider } from "./NostoProvider";
-export { NostoSession } from "./NostoSession"
+export { default as NostoProvider } from "./components/NostoProvider.jsx";
+export { default as NostoSession } from "./components/NostoSession.jsx"
 
 //Export server fetching function:
 export { getNostoData } from './getNostoData.js'
