2c0607de54a7bb51ded2faff2ba94f0f11249495
Timo Westkämper <timo.westkamper@nosto.com>
2024-08-15 14:45:11 +0300

    Reformatted code

--- src/components/NostoProvider.jsx
+++ src/components/NostoProvider.jsx
@@ -1,35 +1,49 @@
-import {NostoProvider as NostoComponent} from "@nosto/nosto-react";
-import {NostoSession} from '@nosto/shopify-hydrogen'
-import {useMatches} from '@remix-run/react'
-import {parseGid, Script} from '@shopify/hydrogen'
+import { NostoProvider as NostoComponent } from "@nosto/nosto-react"
+import { NostoSession } from "@nosto/shopify-hydrogen"
+import { useMatches } from "@remix-run/react"
+import { parseGid, Script } from "@shopify/hydrogen"
 
-export default function ({children, shopifyMarkets: shopifyMarketsProp, ...props}) {
+export default function ({
+  children,
+  shopifyMarkets: shopifyMarketsProp,
+  ...props
+}) {
+  //Get nostoData from root remix loader:
+  const [root] = useMatches()
+  const { language } = root?.data?.selectedLocale || {}
+  const { market } = root?.data?.nostoProviderData?.localization?.country || {}
+  const currentVariation =
+    props?.currentVariation || root?.data?.selectedLocale?.currency
+  const { id: marketId } = parseGid(market?.id)
 
-    //Get nostoData from root remix loader:
-    const [root] = useMatches();
-    const {language} = root?.data?.selectedLocale || {}
-    const {market} = root?.data?.nostoProviderData?.localization?.country || {}
-    const currentVariation = props?.currentVariation || root?.data?.selectedLocale?.currency;
-    const {id: marketId} = parseGid(market?.id)
+  const shopifyMarkets = {
+    marketId: shopifyMarketsProp?.marketId || marketId,
+    language: shopifyMarketsProp?.language || language,
+  }
 
-    const shopifyMarkets = {
-        marketId: shopifyMarketsProp?.marketId || marketId,
-        language: shopifyMarketsProp?.language || language
-    }
+  const scriptUrl =
+    "//connect.nosto.com/script/shopify/market/nosto.js?merchant=" +
+    props.account +
+    "&market=" +
+    (shopifyMarkets?.marketId ? shopifyMarkets?.marketId : "") +
+    "&locale=" +
+    (shopifyMarkets?.language ? shopifyMarkets.language : "")
+  console.log("acc is:" + props.account)
+  console.log("market is:" + JSON.stringify(shopifyMarkets))
+  console.log("script is:" + scriptUrl)
 
-    const scriptUrl = "//connect.nosto.com/script/shopify/market/nosto.js?merchant=" + props.account + "&market=" + (shopifyMarkets?.marketId ? shopifyMarkets?.marketId : "") + "&locale=" + (shopifyMarkets?.language ? shopifyMarkets.language : "");
-    console.log("acc is:" + props.account)
-    console.log("market is:" + JSON.stringify(shopifyMarkets))
-    console.log("script is:" + scriptUrl)
-
-    return (
-        <>
-            <NostoComponent {...props} shopifyMarkets={shopifyMarkets} currentVariation={currentVariation}
-                            loadScript={false}>
-                <NostoSession/>
-                {children}
-                <Script src={scriptUrl}></Script>
-            </NostoComponent>
-        </>
-    )
+  return (
+    <>
+      <NostoComponent
+        {...props}
+        shopifyMarkets={shopifyMarkets}
+        currentVariation={currentVariation}
+        loadScript={false}
+      >
+        <NostoSession />
+        {children}
+        <Script src={scriptUrl}></Script>
+      </NostoComponent>
+    </>
+  )
 }
