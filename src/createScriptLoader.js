bdac90903c331e4f8b442c1298f1ea64a3d91d1f
Mikko Poutanen <12759991+nosto-mikpou@users.noreply.github.com>
2024-08-20 10:21:59 +0300

    Add comment for the createScriptLoader,  support appending script element to <head> if needed

--- src/createScriptLoader.js
+++ src/createScriptLoader.js
@@ -1,5 +1,12 @@
+/**
+* Creates a custom script loader function which passed to nosto-react component NostoProvider as a prop
+* and used in useLoadClientScript hook for loading scripts. This will override the default script loader behaviour
+* since a `nonce` attribute is required by Shopify for third party scripts. Additionally, this lets the useLoadClientScript
+* to handle building the URL string (scriptSrc in the returned function param).
+*/
+
 export default function (nonce) {
-  return function(scriptSrc, options) {
+  return function (scriptSrc, options) {
     return new Promise((resolve, reject) => {
       const script = document.createElement("script")
       script.type = "text/javascript"
@@ -9,7 +16,11 @@ export default function (nonce) {
       script.onload = () => resolve()
       script.onerror = () => reject()
       Object.entries(options?.attributes ?? {}).forEach(([k, v]) => script.setAttribute(k, v))
-      document.body.appendChild(script)
+      if (options?.position === "head") {
+        document.head.appendChild(script)
+      } else {
+        document.body.appendChild(script)
+      }
     })
   }
 }
