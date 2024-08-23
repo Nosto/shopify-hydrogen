cf817a652ca33ad7a617c9752698c72c0722d76a
Ilja Ganulevics <ilja.ganulevics@nosto.com>
2024-08-23 12:31:57 +0300

    testing

--- src/components/NostoProvider.jsx
+++ src/components/NostoProvider.jsx
@@ -1,8 +1,8 @@
-import { NostoProvider as NostoComponent } from "@nosto/nosto-react"
-import { NostoSession } from "@nosto/shopify-hydrogen"
-import { useMatches } from "@remix-run/react"
-import { parseGid, useNonce } from "@shopify/hydrogen"
-import { createScriptLoader } from "../createScriptLoader"
+import {NostoProvider as NostoComponent} from "@nosto/nosto-react"
+import {NostoSession} from "@nosto/shopify-hydrogen"
+import {useMatches} from "@remix-run/react"
+import {parseGid, useNonce} from "@shopify/hydrogen"
+import createScriptLoader from "../createScriptLoader.js";
 
 export default function ({
   children,
@@ -13,12 +13,12 @@ export default function ({
   const [root] = useMatches()
   const { language } = root?.data?.selectedLocale || {}
   const { market } = root?.data?.nostoProviderData?.localization?.country || {}
-  
+
   // useNonce() hook returns CSP nonce. Used to add a nonce to Nosto script via scriptLoader
   // https://shopify.dev/docs/api/hydrogen/2024-07/hooks/usenonce
   const nonce = useNonce()
   const scriptLoader = createScriptLoader(nonce)
-  
+
   const currentVariation =
     props?.currentVariation || root?.data?.selectedLocale?.currency
   const { id: marketId } = parseGid(market?.id)
