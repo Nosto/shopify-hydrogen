8ae73f6a98c45e2e840f4a44ae03431923766be1
Ilja Ganulevics <ilja.ganulevics@nosto.com>
2024-09-03 16:51:37 +0300

    remapping tagging object within NostoProduct

--- src/components/NostoProduct.jsx
+++ src/components/NostoProduct.jsx
@@ -1,6 +1,10 @@
-import { useNostoProduct } from "@nosto/nosto-react"
+import {useNostoProduct} from "@nosto/nosto-react"
 
 export default function NostoProduct(props) {
-  useNostoProduct(props)    
-  return null
+    const {selectedVariant} = props.tagging
+    useNostoProduct({
+        product: props.product,
+        tagging: {product_id: props.product, selected_sku_id: selectedVariant?.sku}
+    })
+    return null
 }
