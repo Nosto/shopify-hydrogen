eb2c0766d44c78cae408d78d2edac36af61a3eb0
Ilja Ganulevics <ilja.ganulevics@nosto.com>
2024-09-11 09:19:01 +0300

    updating nosto-react

--- src/components/NostoProduct.jsx
+++ src/components/NostoProduct.jsx
@@ -1,10 +1,10 @@
-import {useNostoProduct} from "@nosto/nosto-react"
+import { useNostoProduct } from "@nosto/nosto-react"
 
 export default function NostoProduct(props) {
-    const {selectedVariant} = props.tagging
+    const { selectedVariant } = props.tagging
     useNostoProduct({
         product: props.product,
-        tagging: {product_id: props.product, selected_sku_id: selectedVariant?.sku}
+        tagging: { product_id: props.product, selected_sku_id: selectedVariant?.sku }
     })
     return null
 }
